0a15daecac394f79185a0260d4a6c18ccc19cf5b
Mike C <mjcorm@gmail.com>
2017-12-14 05:47:01 -0800

    Removed dupe pallet and characterh labels

--- init.asm
+++ init.asm
@@ -196,30 +196,4 @@ VDPRegisters:
 	dc.b $00 ; 21: DMA source address lo byte
 	dc.b $00 ; 22: DMA source address mid byte
 	dc.b $00 ; 23: DMA source address hi byte, memory-to-VRAM mode (bits 6-7)
-Palette:
-	dc.w $0000 ; Colour 0 - Transparent
-	dc.w $000E ; Colour 1 - Red
-	dc.w $00E0 ; Colour 2 - Green
-	dc.w $0E00 ; Colour 3 - Blue
-	dc.w $0000 ; Colour 4 - Black
-	dc.w $0EEE ; Colour 5 - White
-	dc.w $00EE ; Colour 6 - Yellow
-	dc.w $008E ; Colour 7 - Orange
-	dc.w $0E0E ; Colour 8 - Pink
-	dc.w $0808 ; Colour 9 - Purple
-	dc.w $0444 ; Colour A - Dark grey
-	dc.w $0888 ; Colour B - Light grey
-	dc.w $0EE0 ; Colour C - Turquoise
-	dc.w $000A ; Colour D - Maroon
-	dc.w $0600 ; Colour E - Navy blue
-	dc.w $0060 ; Colour F - Dark green
-CharacterH:
-	dc.l $11000110
-	dc.l $11000110
-	dc.l $11000110
-	dc.l $11111110
-	dc.l $11000110
-	dc.l $11000110
-	dc.l $11000110
-	dc.l $00000000
 __end    ; Very last line, end of ROM address
